60f84c9c99f6f8a64ce90cc048a566770dcbdb6e
jendahejna <xhejna02@vutbr.cz>
2025-01-17 23:06:53 +0100

    Preprocessing changes for NN prediction

--- data_processing/ml_modeling.py
+++ data_processing/ml_modeling.py
@@ -1,6 +1,9 @@
 import joblib
+from tensorflow.keras.models import load_model
+import joblib
 
 
+scaler = joblib.load("neural/scaler.joblib")
 # Further development: Training and adding more ML models
 def predict_temperature(df):
     X = df.drop(["Time", "Latitude", "Longitude", "IP", "Link_ID", "Hour"], axis=1)
@@ -13,3 +16,19 @@ def predict_temperature(df):
         .reset_index()
     )
     return df
+
+def temperature_predict(df):
+    col_order = ['Temperature_MW', 'sun', 'Hour', 'Day', 'Signal', 'Azimuth']
+    X = df[col_order]
+    X_scaled = scaler.transform(X)
+    X_reshaped = X_scaled.reshape((X_scaled.shape[0], X_scaled.shape[1], 1))
+    model = load_model("neural/best_lstm_model.h5", compile=False)
+    predicted_temperatures = model.predict(X_reshaped).flatten()
+    df["Predicted_Temperature"] = predicted_temperatures
+    df = (
+        df.groupby(["Hour", "IP", "Latitude", "Longitude"])["Predicted_Temperature"]
+        .mean()
+        .reset_index()
+    )
+    return df
+
